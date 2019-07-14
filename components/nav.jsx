006d2e51fc7051d0d0d118f44a53734ef6759a06
ALex_Nti <a.ntitoras@gmail.com>
2019-07-14 23:39:36 +0300

    end of 26

--- components/nav.jsx
+++ components/nav.jsx
@@ -1,10 +1,19 @@
 import Link from "next/link";
 import React from "react";
 
+import User from "../components/User";
 import NavStyles from "./styles/NavStyles";
 
 const Nav = () => (
   <NavStyles>
+    <User>
+      {({data:{ me }})=>{
+        console.log(me);
+
+        return <p>User</p>
+      }}
+
+    </User>
     <Link href="/items">
       <a>Shop</a>
     </Link>
